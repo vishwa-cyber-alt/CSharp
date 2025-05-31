91ccba8e6dc37e61803884996180664a3046e391
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-05-31 19:25:06 +0530

    Create 2.cs

---
+++ 2.cs
@@ -0,0 +1,11 @@
+using System;
+class One
+{
+    public static void Main(string[] args)
+    {
+        int id=Convert.ToInt32(Console.ReadLine());
+        string name=Console.ReadLine();
+        Console.WriteLine("ID: "+id);
+        Console.WriteLine("Name: "+name);
+    }
+}
