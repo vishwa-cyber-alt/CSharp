3ce6f1afee0c72cae53da3f867b8354a84acb5de
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-06-01 21:18:55 +0530

    Create 3.cs

---
+++ 3.cs
@@ -0,0 +1,12 @@
+using System;
+class One
+{
+    int id=2;
+    string name="fddf";
+    public static void Main(string[] args)
+    {
+        One a=new One();
+        Console.WriteLine(a.id);
+        Console.WriteLine(a.name);
+    }
+}
